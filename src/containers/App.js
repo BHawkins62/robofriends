f24a47da352ba47ff9df37b68e896095bcf10258
BHawkins62 <bruce@kinhawkins.com>
2021-04-21 21:25:42 -0400

    Robots Internal File Instead

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
+import {robots} from '../robots';
 import './App.css';
 
 
@@ -16,9 +17,7 @@ class App extends Component {
     }
 
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then (response => response.json())
-            .then (users => this.setState({ robots: users}));
+        this.setState({ robots: robots})
     }
 
     onSearchChange = (event) => {
